c5889166850cce8e18d6601e5a7ac92673db1eec
ArunPratap Singh <ArunPratap.Singh@iDeaS.com>
2020-05-10 22:31:29 +0530

    * Creating S3 policy for attaching to lambda role. * Create role for lambda. * Attach required policy for lambda role.

--- aws/chat-app/main.tf
+++ aws/chat-app/main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 resource "aws_s3_bucket" "chat_bucket" {
   bucket = var.chat_domain
   region = var.region
-  acl = "public-read"
+  acl    = "public-read"
   policy = <<POLICY
 {
   "Version": "2012-10-17",
@@ -30,26 +30,26 @@ POLICY
 }
 
 resource "aws_s3_bucket_object" "index_html" {
-  bucket = aws_s3_bucket.chat_bucket.bucket
-  key = "index.html"
+  bucket       = aws_s3_bucket.chat_bucket.bucket
+  key          = "index.html"
   content_type = "text/html"
-  source = "index.html"
+  source       = "index.html"
 }
 
 resource "aws_s3_bucket_object" "error_html" {
-  bucket = aws_s3_bucket.chat_bucket.bucket
-  key = "error.html"
+  bucket       = aws_s3_bucket.chat_bucket.bucket
+  key          = "error.html"
   content_type = "text/html"
-  source = "error.html"
+  source       = "error.html"
 }
 
 resource "aws_s3_bucket_object" "upload_web_app" {
-  bucket = aws_s3_bucket.chat_bucket.bucket
+  bucket   = aws_s3_bucket.chat_bucket.bucket
   for_each = fileset("${path.module}/web-app", "**")
-  key = each.value
-  source = "${path.module}/web-app/${each.value}"
+  key      = each.value
+  source   = "${path.module}/web-app/${each.value}"
   # tracks the versoning identify any changes in file's for upload
-  etag = filemd5("${path.module}/web-app/${each.value}")
+  etag         = filemd5("${path.module}/web-app/${each.value}")
   content_type = "text/html"
 }
 
@@ -58,3 +58,53 @@ output "web_app_resources_uploaded" {
 }
 
 # --- end setting up S3 bucket resources. -------
+
+# --- start create policy/role for lambda -------
+resource "aws_iam_policy" "s3_policy" {
+  name        = "lets-chat-s3-access"
+  description = "lets-chat-s3-access"
+  policy      = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Effect": "Allow",
+      "Resource": ["arn:aws:s3:::${aws_s3_bucket.chat_bucket.bucket}/*" ]
+    }
+  ]
+}
+EOF
+}
+#####
+resource "aws_iam_role" "lambda_role" {
+  name = "lets-chat-lambda-data"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_attach_policy_s3" {
+  role = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.s3_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_attach_policy_basicExecutionRole" {
+  role = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
